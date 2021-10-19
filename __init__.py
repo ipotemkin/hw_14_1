24dba5d15aab4b20e11eae61b1ea76631a382b37
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-10-19 11:19:07 +0300

    added a routes module

--- __init__.py
+++ __init__.py
@@ -4,3 +4,5 @@ app = Flask(__name__)
 app.config['MAX_ITEMS'] = 100
 app.config['DB_FILE'] = 'netflix.db'
 app.config['CATEGORIES'] = {'children': ['G'], 'family': ['PG', 'PG-13'], 'adult': ['R', 'NC-17']}
+
+from routes import *  # noqa
